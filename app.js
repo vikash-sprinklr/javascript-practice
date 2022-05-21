0d37301d0340467e809cdaa15c5822f888b12f8c
Vikash@sprinklr0901 <vikash@sprinklr.com>
2022-05-21 23:41:01 +0530

    promise challenge

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
-console.log("hi there")
-
-function check(){
-    setTimeout(()=>{
-        console.log("from loop")
-    },0)
-}
-
-console.log("end")
+const fakePeople = [
+    { name: 'Rudolph', hasPets: false, currentTemp: 98.6 },
+    { name: 'Zebulon', hasPets: true, currentTemp: 22.6 },
+    { name: 'Harold', hasPets: true, currentTemp: 98.3 },
+  ]
+  
+  const fakeAPICall = (i) => {
+    const returnTime = Math.floor(Math.random() * 1000);
+    return new Promise((resolve, reject) => {
+      if (i >= 0 && i < fakePeople.length) {
+        setTimeout(() => resolve(fakePeople[i]), returnTime);
+      } else {
+        reject({ message: "index out of range" });
+      }
+    });
+  };
+  
+  function getAllData() {
+    // CODE GOES HERE
+    var promiseArray = [0,1,2].map((ele)=>{
+      return fakeAPICall(ele)
+    })
+    
+    var data = Promise.all(promiseArray).then((valueArray)=>{
+      console.log(valueArray)
+      return valueArray
+    })
+    
+    // console.log(data)
+  }
+getAllData()
---
+++ promise challenge.js
@@ -0,0 +1,117 @@
+// Challenge 1
+
+function sayHello() {
+	setTimeout(()=>{
+		  console.log('Hello!')
+  },1000)
+}
+
+// Uncomment the line below when ready
+sayHello(); // should log "Hello" after 1000ms
+
+
+// Challenge 2
+var promise = new Promise(function (resolve, reject) {
+    // ADD CODE HERE
+    setTimeout(()=>{
+        // console.log(resolve)
+        // console.log(reject)
+        resolve('Resolved!')
+    },1000)
+  });
+
+promise.then((data)=>{
+    console.log(data)
+})
+
+
+// Challenge 3
+
+var promise = new Promise(function (resolve, reject) {
+    // ADD CODE HERE
+    reject('Rejected!')
+  });
+
+promise.catch((data)=>{
+    console.log(data)
+})
+
+
+// Challenge 4
+
+promise = new Promise(function (resolve, reject) {
+  // ADD CODE HERE
+  resolve('Promise has been resolved')
+});
+
+// Uncomment the lines below when ready
+promise.then(() => console.log('Promise has been resolved!'));
+console.log("I'm not the promise!");
+
+// Challenge 5
+function delay(){
+	var promise = new Promise((resolve, reject)=>{
+        return setTimeout(()=>{
+            resolve()
+        },1000)
+    })
+
+    return promise
+}
+
+
+// Uncomment the code below to test
+// This code should log "Hello" after 1000ms
+delay().then(sayHello);
+
+
+// Challenge 6
+
+var secondPromise = new Promise((resolve, reject)=>{
+    return resolve('Second!')
+})
+var firstPromise = new Promise((resolve, reject)=>{
+    // console.log(secondPromise)
+    return resolve(secondPromise)
+})
+
+firstPromise.then((pr)=>{
+    return pr
+})
+.then((data)=>{
+    console.log(data)
+})
+
+
+// Challenge 7
+const fakePeople = [
+  { name: 'Rudolph', hasPets: false, currentTemp: 98.6 },
+  { name: 'Zebulon', hasPets: true, currentTemp: 22.6 },
+  { name: 'Harold', hasPets: true, currentTemp: 98.3 },
+]
+
+const fakeAPICall = (i) => {
+  const returnTime = Math.floor(Math.random() * 1000);
+  return new Promise((resolve, reject) => {
+    if (i >= 0 && i < fakePeople.length) {
+      setTimeout(() => resolve(fakePeople[i]), returnTime);
+    } else {
+      reject({ message: "index out of range" });
+    }
+  });
+};
+
+function getAllData() {
+  // CODE GOES HERE
+  var promiseArray = [0,1,2].map((ele)=>{
+    return fakeAPICall(ele)
+  })
+  
+  var data = Promise.all(promiseArray).then((valueArray)=>{
+    console.log(valueArray)
+    return valueArray
+  })
+  
+}
+
+getAllData()
